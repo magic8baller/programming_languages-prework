1ed02bd374d63ebb08ec1ead1813d1a376d4e6d4
Leslie Turis <lturis@gmail.com>
2018-09-11 20:00:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,10 +10,10 @@ def reformat_languages(languages)
 
       type_hash[:type] = type
       #type = string value of type_hash[:type]
-      
+
       if !new_hash[lang_name] #if new_hash doesn't have a language yet, add lang with hash value of type and style
         new_hash[lang_name] = {:type => type, :style => [style]}
-        
+
       #if new_hash has a lang already, just add/push style symbols into an array as values of a :style key
       else new_hash[lang_name][:style] << style
     binding.binding.pry
