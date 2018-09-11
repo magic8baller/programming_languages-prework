69128fb67d7fd89784307f779205cd18d87ec268
Leslie Turis <lturis@gmail.com>
2018-09-11 20:08:49 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,14 +9,14 @@ def reformat_languages(languages)
     # :ruby => {:type => 'interpreted'}
 
       type = type_hash[:type]
-      #type = str val
+      #type = 'interpreted'
 
       if !new_hash[lang_name]  #if new_hash doesn't have a language yet
 
         new_hash[lang_name] = {:type => type, :style => [style]}
           #add lang with hash val of type and style pairs
 
-      #if new_hash has a lang already:
+      #new_hash already has lang?
       else new_hash[lang_name][:style] << style
      # add/push style :symbols --> array as vals of a :style key
 
