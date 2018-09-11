1a4b130fc7dcca16fbd805614319059320a4f94d
Leslie Turis <lturis@gmail.com>
2018-09-11 20:00:37 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,29 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+
+  new_hash = {}
+
+  languages.each do |style, lang_hash|
+   # :oo => {:ruby => { :type => 'interpreted'}, ...}
+    lang_hash.each do |lang_name, type_hash|
+    # :ruby => {:type => 'interpreted'}
+
+      type_hash[:type] = type
+      #type = string value of type_hash[:type]
+      
+      if !new_hash[lang_name] #if new_hash doesn't have a language yet, add lang with hash value of type and style
+        new_hash[lang_name] = {:type => type, :style => [style]}
+        
+      #if new_hash has a lang already, just add/push style symbols into an array as values of a :style key
+      else new_hash[lang_name][:style] << style
+    binding.binding.pry
+
+      end
+
+    end
+
+  end
+
+  new_hash
+
 end
